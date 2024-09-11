4c36f458225bce9313cf63d177c73ef3bb4d2ab1
sam06tanb <samirtajra53@gmail.com>
2024-09-11 19:08:20 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/samir2/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.samir2.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.samir2.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+	
+}
