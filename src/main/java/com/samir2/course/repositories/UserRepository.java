3d52f9a84339afa03fb30ee1899741b8f8e1419d
sam06tanb <samirtajra53@gmail.com>
2024-09-11 19:50:32 -0300

    Service layer, component registration

--- src/main/java/com/samir2/course/repositories/UserRepository.java
+++ src/main/java/com/samir2/course/repositories/UserRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.samir2.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
-	
-	
 }
---
+++ src/main/java/com/samir2/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.samir2.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.samir2.course.entities.User;
+import com.samir2.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findAById(Long Id) {
+	    Optional<User> obj  = repository.findById(Id);
+	    return obj.get();
+	}
+
+}
