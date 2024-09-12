7f867c5888a06e2219403a0bbcea9d81724e7d45
sam06tanb <samirtajra53@gmail.com>
2024-09-12 19:53:26 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/samir2/course/entities/User.java
+++ src/main/java/com/samir2/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.samir2.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,11 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
+	
 	
 	public User() {
 	}
@@ -73,6 +81,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -91,7 +103,5 @@ public class User implements Serializable {
 		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
 				&& Objects.equals(password, other.password) && Objects.equals(phone, other.phone);
 	}
-	
-	
 
 }
