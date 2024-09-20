c71476408710551a8d3e3d496598fcdc5e4b9af7
sam06tanb <samirtajra53@gmail.com>
2024-09-19 23:23:48 -0300

    User insert

--- src/main/java/com/samir2/course/services/UserService.java
+++ src/main/java/com/samir2/course/services/UserService.java
@@ -23,5 +23,9 @@ public class UserService {
 	    Optional<User> obj  = repository.findById(Id);
 	    return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
 
 }
